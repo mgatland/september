1ec380601850f7b4f3765d115c3bcd0f99890859
Matthew Gatland <mgatland@gmail.com>
2019-09-30 18:40:54 +1300

    July game with fixed camera + smaller map

---
+++ client.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+'use strict'
+
+import { game } from './game.js'
+
+function connect () {
+  game.start(function () {})
+}
+
+connect()
---
+++ sounds/sound-convert.bat
@@ -0,0 +1,10 @@
+ffmpeg -i enemyshoot.wav -acodec pcm_s16le enemyshoot_.wav
+ffmpeg -i exp.wav -acodec pcm_s16le exp_.wav
+ffmpeg -i exp2.wav -acodec pcm_s16le exp2_.wav
+ffmpeg -i heal.wav -acodec pcm_s16le heal_.wav
+ffmpeg -i hit2.wav -acodec pcm_s16le hit2_.wav
+ffmpeg -i shoot.wav -acodec pcm_s16le shoot_.wav
+ffmpeg -i playerexp.wav -acodec pcm_s16le playerexp_.wav
+ffmpeg -i playerhit.wav -acodec pcm_s16le playerhit_.wav
+ffmpeg -i hitwall.wav -acodec pcm_s16le hitwall_.wav
+ffmpeg -i spawn.wav -acodec pcm_s16le spawn_.wav
